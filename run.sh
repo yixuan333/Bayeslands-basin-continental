d52d3e66cae4dc98adc8570b0968de58a6097b87
Danial Azam <dazam92@gmail.com>
2019-01-30 14:26:18 +1100

    Adding Bayeslands and ptBayesland scripts

---
+++ run.sh
@@ -0,0 +1,30 @@
+
+#!/bin/sh 
+echo Running all 	
+
+# python ptBayeslands_sedvec.py -p 3 -s 600 -r 10 -b 0.05 -pt 0.5
+
+  
+ 
+for x in  1
+	do
+		for prob in 6
+		do
+			
+			python ptBayeslands_sedvec.py -p $prob -s 50000 -r 20 -t 10 -swap 0.01 -b 0.25 -pt 0.5
+ 
+ 
+		done
+	done 
+
+
+
+
+
+#python ptBayeslands_sedvec.py -p 6 -s 10000 -r 10 -b 0.10 -pt 0.5
+
+#python ptBayeslands_sedvec.py -p 4 -s 10000 -r 10 -b 0.05 -pt 0.7
+#python ptBayeslands_sedvec.py -p 6 -s 10000 -r 10 -b 0.10 -pt 0.7
+
+#python ptBayeslands_sedvec.py -p 5 -s 10000 -r 10 -b 0.15 -pt 0.7
+#python ptBayeslands_sedvec.py -p 5 -s 10000 -r 10 -b 0.15 -pt 0.5
