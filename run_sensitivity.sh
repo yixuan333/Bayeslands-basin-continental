d4e145f835d8194e6c7cb33177815df931107eb3
Danial Azam <dazam92@gmail.com>
2019-12-10 15:26:50 +1100

    Merge conflicts removed

--- run_sensitivity.sh
+++ run_sensitivity.sh
@@ -2,26 +2,9 @@
 #!/bin/bash  
 echo Running all 	 
 
-<<<<<<< HEAD
-# python sensitivity_analysis.py -vstart 0 -vend 15
-=======
-<<<<<<< HEAD
-# python sensitivity_analysis.py -vstart 0 -vend 15
-
->>>>>>> master
-python sensitivity_analysis.py -vstart 0 -vend 3 & 
-python sensitivity_analysis.py -vstart 3 -vend 6 &
-python sensitivity_analysis.py -vstart 6 -vend 9 &
-python sensitivity_analysis.py -vstart 9 -vend 12 &
-python sensitivity_analysis.py -vstart 12 -vend 15 &
-<<<<<<< HEAD
-=======
-=======
-python sensitivity_analysis.py -vstart 12 -vend 13
-# python sensitivity_analysis.py -vstart 0 -vend 3 & 
+python sensitivity_analysis.py -vstart 0 -vend 3 
 # python sensitivity_analysis.py -vstart 3 -vend 6 &
 # python sensitivity_analysis.py -vstart 6 -vend 9 &
 # python sensitivity_analysis.py -vstart 9 -vend 12 &
 # python sensitivity_analysis.py -vstart 12 -vend 15 &
->>>>>>> master
->>>>>>> master
+
